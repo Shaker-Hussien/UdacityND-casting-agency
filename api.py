cd927650b322a16cf8207d1e1148347fba03c9f7
ShakerHussien <shakerhussiencmp@gmail.com>
2020-12-17 05:01:34 +0200

    *edit api file

--- api.py
+++ api.py
@@ -3,34 +3,28 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
-
-from dotenv import load_dotenv
-load_dotenv()
-
 from database.models import setup_db, db_drop_and_create_all
-
 
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-#db_drop_and_create_all()
 
 # Routes
 @app.route('/')
-@app.route('/login-result')
 def home():
     return jsonify({
-        "application name" : "Casting Agency APIs",
+        "application name": "Casting Agency APIs",
         "owner": "Shaker Hussien"
     })
 
-from routes.actor import *
-from routes.movie import *
+
+from routes.actor import *  # noqa: E402
+from routes.movie import *  # noqa: E402
 
 # Error Handlers
-from error_handlers import *
+from error_handlers import *  # noqa: E402
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
